3553d354eeaadbc80c95f4a8d7c2438fb124e711
illia Voitenko <illia@MacBook-Pro-illia.local>
2023-09-27 17:03:53 +0200

    test routing

---
+++ pages/contacts/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contacts = () => {
+  return <div>contacts list</div>;
+};
+
+export default Contacts;
---
+++ pages/posts.js
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <>
+      <span>posts list :</span>
+      <span>item </span>
+    </>
+  );
+};
+export default Posts;
