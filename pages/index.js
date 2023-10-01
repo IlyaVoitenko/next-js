48c82f7c649c1108fd132010b3f720e626db9cf1
illia Voitenko <illia@mbp-illia.localdomain>
2023-10-01 13:28:24 +0200

    added api for home page and env file

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
-import Heading from "@/components/heading";
 import style from "../styles/Home.module.scss";
 import Head from "next/head";
+import Socials from "@/components/Socials";
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const request = await fetch(`${process.env.HOST_API}/socials`);
+  const response = await request.json();
+
+  if (!response) return { notFound: true };
+
+  return { props: { socials: response } };
+};
+
+const Home = ({ socials }) => {
   return (
     <div className={style.container}>
       <Head>
         <title>Home</title>
       </Head>
-      <Heading text={"hello world"} />
+      <Socials socials={socials} />
     </div>
   );
 };
